4f281cb19a73ee688d8cbbe20dbbb493640603f7
zhaoxinxin <1186037180@qq.com>
2025-03-10 15:04:53 +0800

    feat: artifact adds model type
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- src/portal/src/app/base/project/repository/artifact/artifact-label/artifact-label.component.ts
+++ src/portal/src/app/base/project/repository/artifact/artifact-label/artifact-label.component.ts
@@ -19,6 +19,7 @@ export class ArtifactLabelComponent implements OnInit {
             this.type = this.artifactDetails.type;
         }
     }
+
     capitalizeFirstLetter(text: string): string {
         if (!text) return text;
         return text.charAt(0).toUpperCase() + text.slice(1);
